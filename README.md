174f73a2cadb6ea673bec8e5c5dba29afd02126d
angelikaamor <angelikaamorxo@gmail.com>
2022-01-24 14:38:13 +1030

    still need to fix js code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# weather-dashboard
+# Server-Side APIs: Weather Dashboard
+
+## User Story
+
+## Acceptance Criteria
+
+## The Project
+
+## The Final Product
+
+## Link to url: 
---
+++ assets/script.js
@@ -0,0 +1,181 @@
+// Linking HTML 
+var searchButton = document.querySelector("#searchBtn");
+var weatherSection = document.querySelector("#weatherContent");
+var sideBar = document.querySelector("#sideBar");
+var historyContent = document.querySelector(".history");
+var input = document.querySelector("#input");
+var createHistory;
+var searchHistory = [];
+
+// Variables
+var lon;
+var lat;
+var city;
+var APIKey = "1112e3fcb547812716547c3829826d88";
+
+function init() {
+    getHistory();
+}
+
+function searchApi(query) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL) 
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function (data) {
+            lon = data.coord.lon;
+            lat = data.coord.lat;
+            city = data.name;
+        })
+        .then(function () {
+            let weatherUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=current,hourly,daily&appid=" + APIKey;
+            fetch(weatherUrl)
+                .then(function (response) {
+                    if (!response.ok) {
+                        throw response.json();
+                    }
+                    return response.json();
+                })
+                .then(function (data) {
+                    printResults(data, city);
+                })
+
+        })
+}
+
+function printResults(results, name) {
+
+    weatherSection.innerHTML = "";
+
+    var locationE1 = document.createElement('div');
+    locationE1.classList.add('card');
+    weatherSection.append(locationE1);
+    var locationHeader = document.createElement('div');
+    locationHeader.classList.add('cardHeader');
+    var locationInfo = document.createElement('div');
+    locationInfo.classList.add('card');
+    locationE1.append(locationHeader, locationInfo);
+
+    var title = document.createElement('h2');
+    title.textContent = name;
+    title.classList.add('card-title');
+
+    var weatherImage = document.createElement('img');
+    var iconcode = results.current.weather[0].icon;
+    var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+    weatherImage.setAttribute('src', iconurl);
+    locationHeader.append(title, weatherImage);
+
+    var temp = document.createElement('p');
+    temp.textContent = "Temp: " + results.current.temp + "c";
+
+    var wind = document.createElement('p');
+    wind.textContent = "Wind: " + results.current.wind_speed + "km/h";
+
+    var humidity = document.createElement('p');
+    humidity.textContent = "Humidity: " + results.current.humidity + "%";
+
+    var index = document.createElement('p');
+    index.textContent = "UV Index: " + results.current.uvi;
+    if(results.current.uvi < 3) {
+        index.classList.add("text-success", "fw-bold");
+    } else if (results.current.uvi > 3  && results.curren.uvi < 6) {
+        index.classList.add("text-warning", "fw-bold");
+    } else {
+        index.classList.add("text-danger", "fw-bold");
+    }
+
+    locationInfo.append(temp, wind, humidity, index);
+
+    var forecast = document.createElement('div');
+    forecast.classList.add('d-flex', 'daily');
+    weatherSection.append(forecast);
+    setHistory(name);
+
+    for (var i = 0; i < 5; i++) {
+        var dayContainer = document.createElement('div');
+        dayContainer.classList.add('weekday', 'card');
+        forecast.append(dayContainer);
+
+        var day = document.createElement('div');
+        day.classList.add('card-body', 'forecast');
+
+        var date = document.createElement('h3');
+        date.textContent = moment().add(i + 1, 'days').format('dddd');
+        date.classList.add('card-title');
+        day.append(date);
+
+        var image = document.createElement('img');
+        var code = results.daily[i].weather[0].icon;
+        var imageUrl = "https://openweathermap.org/img/w/" + code + ".png";
+        image.setAttribute('src', imageUrl);
+        date.classList.add('card-text');
+        day.append(image);
+
+        var temp = document.createElement('p');
+        temp.textContent = "Temp: " + results.daily[i].temp.day + 'c';
+
+        var wind = document.createElement('p');
+        wind.textContent = "Wind: " + results.daily[i].wind_speed + 'km/h';
+
+        var humidity = document.createElement('p');
+        humidity.textContent = "Humiditiy: " + results.daily[i].humidity + '%';
+        day.append(humidity);
+        dayContainer.append(day);
+    }
+}
+
+function handleSearchResults() {
+    var inputVal = document.querySelector("#input").value;
+
+    if (!inputVal) {
+        console.error("Try again..");
+        return;
+    }
+    searchApi(inputVal);
+}
+
+function setHistory(name) {
+    if (searchHistory.includes(name) == false) {
+        searchHistory.push(name);
+        localStorage.setItem("location", JSON.stringify(searchHistory));
+        addCity(name);
+    }
+}
+
+function getHistoryFromStorage() {
+    return JSON.parse(localStorage.getItem("location")) || [];
+}
+
+function addCity(city) {
+    createHistory = document.createElement('button');
+    createHistory.classList.add('btn');
+    createHistory.textContent = city;
+    createHistory.setAttribute("city-data", city);
+    historyContent.append(createHistory);
+}
+
+function getHistory() {
+    searchHistory = getHistoryFromStorage();
+    historyContent.innerHTML = "";
+    for (var i = 0; i < searchHistory.length; i++) {
+        addCity(searchHistory[i]);
+    }
+}
+
+init();
+
+searchButton.addEventListener("click", function () {
+    handleSearchResults();
+    getHistory();
+    input.value = "";
+})
+
+historyContent.addEventListener("click", function (event) {
+    searchApi(event.target.dataset.city);
+})
